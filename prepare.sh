9a6b0df90b84d48e62b14013da457b7bbd6169ad
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-03-17 11:36:10 -0500

    update

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+#Install Docker
 ./install-docker.sh
+# Install kubeadm, kubelet, kubectl
 ./install-kubectl-kubelet-kubeadm.sh
+
+# install CA and certs
+# Install cfssl and cfssljson:
+curl -o /usr/local/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+curl -o /usr/local/bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+chmod +x /usr/local/bin/cfssl*
